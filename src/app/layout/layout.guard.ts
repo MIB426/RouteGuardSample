b19de31ce3224d579bbab28aef43960b36a2130a
James Wu <MIB426@hotmail.com>
2019-06-03 21:36:06 +0800

    add Resolver

--- src/app/layout/layout.guard.ts
+++ src/app/layout/layout.guard.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LayoutGuard implements  CanActivate {
+export class LayoutGuard implements CanActivate {
+  constructor(private router: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      const canActivate = next.queryParams.name === 'Leo';
+    const canActivate = next.queryParams.name === 'Leo';
 
-      if (!canActivate) {
-        alert('你不是Leo，不能進去！');
-      }
+    if (!canActivate) {
+      alert('你不是Leo，不能進去！');
+      this.router.navigate(['/login'], {
+        queryParams: {
+          return: state.url
+        }
+      });
 
-      return canActivate;
+    }
+
+    return canActivate;
   }
 }
